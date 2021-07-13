0ef80cd08064890d8da4d3e722a03466a39e194a
sharner <sharner@kth.se>
2021-07-13 15:32:19 +0200

    Started work on color picker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { ChromePicker } from 'react-color';
 
 function App() {
+  const [col1, setCol1] = useState("ff0000");
+  const [showCol1Picker, setShowCol1Picker] = useState(false);
+
+  const col1style = {
+    display: "flex",
+    width: "20px",
+    height: "20px",
+    color: {col1},
+    backgroundColor: {col1},
+    padding: "10px",
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 style={{col1}}>
+        Flag Generator
+      </h1>
+
+      <div className="ColorSelector">
+        <button
+          onClick={() => setShowCol1Picker(showCol1Picker => !showCol1Picker)}
         >
-          Learn React
-        </a>
-      </header>
+          {showCol1Picker ? 'Confirm color 1' : 'Color 1: '}
+        </button>
+        {showCol1Picker && (
+          <ChromePicker
+            color={col1}
+            onChange={ (col1) => {setCol1(col1.hex)} }
+          />
+          )}
+      </div>
+      
+      <div style={col1style}></div>
     </div>
   );
 }
