c7fa3a334d130418edeaf337b864390c26fcc8a0
sharner <sharner@kth.se>
2021-07-15 12:44:24 +0200

    Change if to switch statement

--- src/Flag.js
+++ src/Flag.js
@@ -3,59 +3,62 @@ import { FLAG } from "./constants";
 import { Rect } from "react-konva";
 
 export function Flag({ flagStyle, col1, col2, col3 }) {
-  if (flagStyle === "hor_tricolor") {
-    return (
-      <>
-        <Rect
-          x={FLAG.START_X}
-          y={FLAG.START_Y}
-          width={FLAG.WIDTH / 3}
-          height={FLAG.HEIGHT}
-          fill={col1}
-        />
-        <Rect
-          x={FLAG.START_X + FLAG.WIDTH / 3}
-          y={FLAG.START_Y}
-          width={FLAG.WIDTH / 3}
-          height={FLAG.HEIGHT}
-          fill={col2}
-        />
-        <Rect
-          x={FLAG.START_X + (2 * FLAG.WIDTH) / 3}
-          y={FLAG.START_Y}
-          width={FLAG.WIDTH / 3}
-          height={FLAG.HEIGHT}
-          fill={col3}
-        />
-      </>
-    );
-  } else if (flagStyle === "ver_tricolor") {
-    return (
-      <>
-        <Rect
-          x={FLAG.START_X}
-          y={FLAG.START_Y}
-          width={FLAG.WIDTH}
-          height={FLAG.HEIGHT / 3}
-          fill={col1}
-        />
-        <Rect
-          x={FLAG.START_X}
-          y={FLAG.START_Y + FLAG.HEIGHT / 3}
-          width={FLAG.WIDTH}
-          height={FLAG.HEIGHT / 3}
-          fill={col2}
-        />
-        <Rect
-          x={FLAG.START_X}
-          y={FLAG.START_Y + (2 * FLAG.HEIGHT) / 3}
-          width={FLAG.WIDTH}
-          height={FLAG.HEIGHT / 3}
-          fill={col3}
-        />
-      </>
-    );
-  }
+  switch (flagStyle) {
+    case "hor_tricolor":
+      return (
+        <>
+          <Rect
+            x={FLAG.START_X}
+            y={FLAG.START_Y}
+            width={FLAG.WIDTH / 3}
+            height={FLAG.HEIGHT}
+            fill={col1}
+          />
+          <Rect
+            x={FLAG.START_X + FLAG.WIDTH / 3}
+            y={FLAG.START_Y}
+            width={FLAG.WIDTH / 3}
+            height={FLAG.HEIGHT}
+            fill={col2}
+          />
+          <Rect
+            x={FLAG.START_X + (2 * FLAG.WIDTH) / 3}
+            y={FLAG.START_Y}
+            width={FLAG.WIDTH / 3}
+            height={FLAG.HEIGHT}
+            fill={col3}
+          />
+        </>
+      );
 
-  return null;
+    case "ver_tricolor":
+      return (
+        <>
+          <Rect
+            x={FLAG.START_X}
+            y={FLAG.START_Y}
+            width={FLAG.WIDTH}
+            height={FLAG.HEIGHT / 3}
+            fill={col1}
+          />
+          <Rect
+            x={FLAG.START_X}
+            y={FLAG.START_Y + FLAG.HEIGHT / 3}
+            width={FLAG.WIDTH}
+            height={FLAG.HEIGHT / 3}
+            fill={col2}
+          />
+          <Rect
+            x={FLAG.START_X}
+            y={FLAG.START_Y + (2 * FLAG.HEIGHT) / 3}
+            width={FLAG.WIDTH}
+            height={FLAG.HEIGHT / 3}
+            fill={col3}
+          />
+        </>
+      );
+
+    default:
+      return null;
+  }
 }
